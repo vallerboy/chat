2fd72191495d9e7de5ccb089444dfa9653be67b1
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-11-26 13:16:01 +0100

    asd

--- src/main/java/pl/oskarpolak/chat/models/WebSocket.java
+++ src/main/java/pl/oskarpolak/chat/models/WebSocket.java
@@ -10,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -19,6 +22,7 @@ import java.util.logging.Logger;
 @Log
 public class WebSocket extends TextWebSocketHandler implements WebSocketConfigurer {
     private List<UserModel> sessions = new LinkedList<>();
+    private Deque<String> lastTenMessages = new ArrayDeque<>();
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
@@ -48,8 +52,14 @@ public class WebSocket extends TextWebSocketHandler implements WebSocketConfigur
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         UserModel sender = findUserBySession(session);
         if(sender.getNickname() == null){
+            if(!isNameFree(message.getPayload())){
+                sender.getSession().sendMessage(new TextMessage("Ten nick jest zajęty"));
+                return;
+            }
             sender.setNickname(message.getPayload()); // jego nick = wiadomosc ktora wyslal
             sender.getSession().sendMessage(new TextMessage("Ustawiłeś swój nick!"));
+
+            sendMessageArchiveToUser(sender);
             return;
         }
 
@@ -57,5 +67,28 @@ public class WebSocket extends TextWebSocketHandler implements WebSocketConfigur
              user.getSession()
                      .sendMessage(new TextMessage(sender.getNickname() + ": " + message.getPayload()));
         }
+
+        addMessageToArchive(sender.getNickname() + ": " + message.getPayload());
+    }
+
+    private void sendMessageArchiveToUser(UserModel sender) throws IOException {
+        for (String lastTenMessage : lastTenMessages) {
+            sender.getSession().sendMessage(new TextMessage(lastTenMessage));
+        }
+    }
+
+    private boolean isNameFree(String nickname){
+        return sessions
+                .stream()
+                .filter(s -> s.getNickname() != null)
+                .noneMatch(s -> s.getNickname().equals(nickname));
+    }
+
+    private void addMessageToArchive(String message){
+        if(lastTenMessages.size() >= 10){
+            lastTenMessages.pollFirst();
+        }
+
+        lastTenMessages.addLast(message);
     }
 }
