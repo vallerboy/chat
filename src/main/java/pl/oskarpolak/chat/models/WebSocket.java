529e96fad090264e98468a057cec2e05157b1549
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-11-26 11:10:48 +0100

    asd

--- src/main/java/pl/oskarpolak/chat/models/WebSocket.java
+++ src/main/java/pl/oskarpolak/chat/models/WebSocket.java
@@ -3,18 +3,23 @@ package pl.oskarpolak.chat.models;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
 
 @Component
 @EnableWebSocket
 @Log
 public class WebSocket extends TextWebSocketHandler implements WebSocketConfigurer {
+    private List<WebSocketSession> sessions = new LinkedList<>();
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry.addHandler(this, "/chat").setAllowedOrigins("*");
@@ -22,11 +27,20 @@ public class WebSocket extends TextWebSocketHandler implements WebSocketConfigur
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
         log.info("Ktoś połączył się  z naszym socketem");
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
         log.info("Ktoś wyszedł");
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession : sessions) {
+             webSocketSession.sendMessage(message);
+        }
     }
 }
